cc0d61ec994b139ca95a8c5e2689d7687cf01f28
tenordiamond <christer.hellkvist@gmail.com>
2019-04-20 15:33:20 +0200

    Create allMyCats.py

---
+++ allMyCats.py
@@ -0,0 +1,13 @@
+#Input validation with isalpha
+catNames = []
+while True:
+	name = input('Enter the name of cat ' + str(len(catNames) +1) + ' (or type quit to stop.):')
+	if name.isalpha():
+		if name == "quit":
+			break
+		catNames = catNames + [name] #concat list
+	else:
+		print("Enter a valid string")
+print('The catnames  you entered are:')
+for name in catNames:
+	print(' ' + name)
