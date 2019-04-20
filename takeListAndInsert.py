ad95f1275f3616549b3bd8fc16bcce8412614bf0
Christer Hellkvist <Christer.Hellkvist@gmail.com>
2019-04-20 21:41:12 +0200

    New training script

---
+++ takeListAndInsert.py
@@ -0,0 +1,7 @@
+#takes a list value as an argument and returns a string with all the items separated by a comma and a space, with and inserted before the last item”
+insertList = ['one', 'two', 'three', 'four']
+
+def doInsertion(theList):
+	return ', '.join(map(str, theList[:-1])) + ' and ' + str(theList[-1])
+
+print(doInsertion(insertList))
